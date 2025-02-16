c28074d0901d5da2a253556e9f2856abdaf82d12
Ngoc <ngockhanhmai1102@gmail.com>
2025-02-16 16:04:45 +0000

    update longest_word_finder.py

--- longest_word_finder.py
+++ longest_word_finder.py
@@ -2,11 +2,11 @@
 # Punctuation is INCLUDED in the word 
 
 def find_longest_word():
-    sentence = input("Your sentence: ")
-    longest = ""
-    for word in sentence.split():
+    sentence = input("Your sentence: ") # Get the user input
+    longest = "" # Set initial longest word to be 0 length
+    for word in sentence.split(): 
         if len(word) > len(longest):
-            longest = word
+            longest = word # If the word is longer than the CURRENT longest, word becomes the longest
     return longest
 
-print(f'The longest word is: {find_longest_word()}')
+print(f'The longest word is: {find_longest_word()}') # Print the result

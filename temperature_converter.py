54cdcc93e4c855fe9ba0a98132adbd131831878b
Ngoc <ngockhanhmai1102@gmail.com>
2025-02-16 14:43:28 +0000

    update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,13 +1,13 @@
 def scale_from():
     scale = None
-    while scale is None:
+    while scale is None: # Repeart the loop until the user enters a valid scale
         scale = input("Which scale are you converting from? (C/F/K): ")
-        if scale != "C" and scale != "F" and scale != "K":
+        if scale != "C" and scale != "F" and scale != "K": # if the scale is not C, F, or K; it is invalid --> repeat the loop
             print("Invalid scale.")
             scale = None
     return scale
 
-def scale_to():
+def scale_to(): 
     scale = None
     while scale is None:
         scale = input("Which scale are you converting to? (C/F/K): ")
@@ -16,11 +16,11 @@ def scale_to():
             scale = None
     return scale
 
-def temperature_converter():
+def temperature_converter(): # Take the scales provided by the user and convert to the desired scale
     scale_from = scale_from()
     scale_to = scale_to()
-    temperature = float(input(f"Enter the temperature in {scale_from}: "))
-    if scale_from == "C" and scale_to == "F":
+    temperature = float(input(f"Enter the temperature in {scale_from}: ")) # Make sure the temperature is a float
+    if scale_from == "C" and scale_to == "F": # Different formulas for different conversions
         result = (temperature * 9/5) + 32
     elif scale_from == "C" and scale_to == "K":
         result = temperature + 273.15
@@ -34,5 +34,5 @@ def temperature_converter():
         result = (temperature - 273.15) * 9/5 + 32
     else:
         result = temperature
-    print(f"The temperature is {result} {scale_to}")
+    print(f"The temperature is {result} {scale_to}") # Print the result
         
